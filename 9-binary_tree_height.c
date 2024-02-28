2196b11cd97528ce5bcf9e3561d5bc16820f400a
Houda Abdellaoui <abdellaouihouda@gmail.com>
2024-02-28 23:32:27 +0100

    Height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+typedef struct binary_tree_s binary_tree_t;
+struct binary_tree_s
+{
+    int n;
+    binary_tree_t *parent;
+    binary_tree_t *left;
+    binary_tree_t *right;
+};
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height
+ *
+ * Return: Height of the binary tree, 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    size_t left_height, right_height;
+
+    if (tree == NULL)
+        return 0;
+
+    left_height = binary_tree_height(tree->left);
+    right_height = binary_tree_height(tree->right);
+
+    return (1 + (left_height > right_height ? left_height : right_height));
+}
